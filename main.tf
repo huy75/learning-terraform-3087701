7abd9148841c26006b0048bac78850a769bbd1dc
Huy Nguyen <42672497+huy75@users.noreply.github.com>
2023-02-22 11:14:28 +0100

    typo corrected

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "blog_sg" {
   ingress_rules       = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"
+  egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
